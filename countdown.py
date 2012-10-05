6747552d8c85e820b1ceb1658d470f590c83fe26
Aimee Langmaid <aimee@aimeeble.com>
2012-10-05 10:08:14 -0700

    add sections; inclusive dates.

--- countdown.py
+++ countdown.py
@@ -6,9 +6,10 @@ import sys
 
 
 class Event(object):
-   def __init__(self, description, when):
+   def __init__(self, description, when, inclusive=False):
       self.description = description
       self.when = when
+      self.inclusive = inclusive
 
    @classmethod
    def from_string(cls, s):
@@ -16,25 +17,69 @@ class Event(object):
       date = parts[0]
       description = ' '.join(parts[1:])
 
+      inclusive = False
+      if date[-1] == '!':
+         inclusive = True
+         date = date[:-1]
+
       try:
          when = datetime.datetime.strptime(date, '%Y-%m-%d').date()
       except ValueError:
          return None
 
-      return Event(description, when)
+      return Event(description, when, inclusive)
 
    def relative_date(self, padding=0):
       '''Returns relative date, such as 5 (if remaining) or -5 (if past).
 
       '''
-      return (self.when - datetime.date.today()).days
+      days = (self.when - datetime.date.today()).days
+      if days > 0:
+         return days + (1 if self.inclusive else 0)
+      else:
+         return days - (1 if self.inclusive else 0)
 
    def __str__(self):
-      return 'event<%s @ %s>' % (self.description, self.when)
+      days = self.relative_date()
+      if days > 0:
+         dir_text = 'left'
+      else:
+         days *= -1
+         dir_text = 'ago'
+
+      return '%(days)4d days %(dir)-4s - %(desc)s' % {
+         'days': days,
+         'dir': dir_text,
+         'desc': self.description,
+      }
+
+class Section(object):
+   def __init__(self, description):
+      self.description = description
+      self.events = []
+
+   def append(self, obj):
+      self.events.append(obj)
+
+   def sort(self):
+      self.events.sort(lambda x,y: (x.when-y.when).days)
+
+   def __len__(self):
+      return len(self.events)
+
+   def __str__(self):
+      res = '-- %s ' % (self.description)
+      res += '-'*(50-len(res))
+      for event in self.events:
+         res += '\n%s' % (str(event))
+      res += '\n'
+      return res
 
 class Countdown(object):
    def __init__(self, filename=None):
-      self.events = []
+      default_section = Section('Default Group')
+      self.sections = [default_section]
+      self.current_section = default_section
 
       with open(filename, "r") as fh:
          raw_data = fh.readlines()
@@ -46,30 +91,29 @@ class Countdown(object):
          if not raw_event or raw_event[0] == '#':
             continue
 
-         event = Event.from_string(raw_event)
-         if not event:
-            raise ValueError("invalid event format: %s" % raw_event)
-         self.events.append(event)
+         # New section
+         if raw_event[0] == '-':
+            sec = Section(raw_event[1:].strip())
+            self.current_section = sec
+            self.sections.append(sec)
 
-      self.events.sort(lambda x,y: (x.when-y.when).days)
+         # New event
+         else:
+            event = Event.from_string(raw_event)
+            if not event:
+               raise ValueError("invalid event format: %s" % raw_event)
+            self.current_section.append(event)
+
+      for sec in self.sections:
+         sec.sort()
 
    def __str__(self):
       res = ''
       nl = ''
-      for event in self.events:
-         days = event.relative_date()
-         if days > 0:
-            dir_text = 'left'
-         else:
-            days *= -1
-            dir_text = 'ago'
-         res += '%(newline)s%(days)4d days %(dir)-5s - %(desc)s' % {
-            'newline': nl,
-            'days': days,
-            'dir': dir_text,
-            'desc': event.description,
-         }
-         nl = '\n'
+      for sec in self.sections:
+         if len(sec):
+            res += '%s%s' % (nl, str(sec))
+            nl = '\n'
       return res
 
 
