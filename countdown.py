f8226eaa660bc34db30f6ba9d83e972a410f3d1c
Aimee Langmaid <aimee@aimeeble.com>
2012-10-07 12:10:51 -0700

    display in days, months, or years.

--- countdown.py
+++ countdown.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
 
+import argparse
 import datetime
 import os
 import sys
+
+
+DISPLAY_DAYS = 1
+DISPLAY_MONTHS = 2
+DISPLAY_YEARS = 3
 
 
 class Event(object):
@@ -29,7 +35,7 @@ class Event(object):
 
       return Event(description, when, inclusive)
 
-   def relative_date(self, padding=0):
+   def relative_days(self):
       '''Returns relative date, such as 5 (if remaining) or -5 (if past).
 
       '''
@@ -39,19 +45,38 @@ class Event(object):
       else:
          return days - (1 if self.inclusive else 0)
 
-   def __str__(self):
-      days = self.relative_date()
-      if days > 0:
+   def relative_months(self):
+      days = self.relative_days()
+      return float(days) / 31.
+
+   def relative_years(self):
+      days = self.relative_days()
+      return float(days) / 365.
+
+   def to_string(self, display):
+      # Get the value
+      if display == DISPLAY_DAYS:
+         delta = self.relative_days()
+      elif display == DISPLAY_MONTHS:
+         delta = self.relative_months()
+      elif display == DISPLAY_YEARS:
+         delta = self.relative_years()
+
+      if delta > 0:
          dir_text = 'left'
       else:
-         days *= -1
+         delta *= -1
          dir_text = 'ago'
 
-      return '%(days)4d days %(dir)-4s - %(desc)s' % {
-         'days': days,
-         'dir': dir_text,
-         'desc': self.description,
-      }
+      # Format it
+      if display == DISPLAY_DAYS:
+         delta = '%6u days %-4s' % (delta, dir_text)
+      elif display == DISPLAY_MONTHS:
+         delta = '%5.2f months %-4s' % (delta, dir_text)
+      elif display == DISPLAY_YEARS:
+         delta = '%5.2f years %-4s' % (delta, dir_text)
+
+      return '%s - %s' % (delta, self.description)
 
 class Section(object):
    def __init__(self, description):
@@ -67,19 +92,24 @@ class Section(object):
    def __len__(self):
       return len(self.events)
 
-   def __str__(self):
+   def to_string(self, display):
       res = '-- %s ' % (self.description)
       res += '-'*(50-len(res))
       for event in self.events:
-         res += '\n%s' % (str(event))
+         res += '\n%s' % (event.to_string(display))
       res += '\n'
       return res
 
 class Countdown(object):
-   def __init__(self, filename=None):
+   def __init__(self, filename, display):
       default_section = Section('Default Group')
       self.sections = [default_section]
       self.current_section = default_section
+
+      if display:
+         self.display = display
+      else:
+         self.display = DISPLAY_DAYS
 
       with open(filename, "r") as fh:
          raw_data = fh.readlines()
@@ -112,16 +142,26 @@ class Countdown(object):
       nl = ''
       for sec in self.sections:
          if len(sec):
-            res += '%s%s' % (nl, str(sec))
+            res += '%s%s' % (nl, sec.to_string(self.display))
             nl = '\n'
       return res
 
 
 if __name__ == '__main__':
-   if len(sys.argv) > 1:
-      filename = sys.argv[1]
+   parser = argparse.ArgumentParser()
+   group = parser.add_mutually_exclusive_group()
+   group.add_argument('-d', action='store_const',const=DISPLAY_DAYS,
+         dest='display', help='display in days')
+   group.add_argument('-m', action='store_const', const=DISPLAY_MONTHS,
+         dest='display', help='display in months')
+   group.add_argument('-y', action='store_const', const=DISPLAY_YEARS,
+         dest='display', help='display in years')
+   args, rest = parser.parse_known_args()
+
+   if len(rest):
+      filename = rest[0]
    else:
       filename = os.path.join(os.environ.get('HOME'), '.countdownrc')
-   c = Countdown(filename)
+   c = Countdown(filename, args.display)
 
    print c
